4601d446f257bd872555b835329ce25ed8bac366
Ekam Singh Chahal <ecsingh1997@gmail.com>
2018-06-21 16:05:49 +0530

    individual todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose.js');
 const {todo} = require('./models/todo.js');
@@ -26,6 +27,20 @@ app.get('/todos',(req,res) => {
   },(err) => {
     res.send(err);
   });
+});
+
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send('Not a valid ID');
+  }
+
+  todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send('No todos found');
+    }
+    res.status(200).send({todo});
+  }).catch((err) => res.status(400).send());
 });
 
 app.listen(3000,() => {
