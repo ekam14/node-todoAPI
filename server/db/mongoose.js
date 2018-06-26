2621900cd81d72e3fb0709decf0ac9810901c72b
Ekam Singh Chahal <ecsingh1997@gmail.com>
2018-06-26 19:13:16 +0530

    updates

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const {ObjectID} = require('mongodb');
 
 mongoose.Promise = global.Promise;  //telling the mongoose that we will use promises instead of callback functions
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://<Ekam>:<Ekam1997>@ds115971.mlab.com:15971/todoapp');  //TodoApp is the database
+mongoose.connect(process.env.MONGODB_URI);  //TodoApp is the database
 
 module.exports = {mongoose};
